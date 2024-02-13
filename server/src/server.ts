9c297017a8d609904941a95115599206c1f7de15
P4ND4P0W3R <paul.hu@efrei.net>
2024-02-13 20:28:30 +0100

    feat(server): Create Express server
    Created an Express server with a .env file

---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello, Express with TypeScript!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Access the server at http://localhost:${PORT}`)
+});
