f55fc371925aa30367764e114681f5d92d33b0c6
Cyrilnkl <cyrilnkl5@gmail.com>
2024-02-21 16:34:39 +0100

    feat-modal choice v2.1 - message choice

--- client/src/components/ModalConfirmStatus.tsx
+++ client/src/components/ModalConfirmStatus.tsx
@@ -9,12 +9,20 @@ export const ModalConfirmStatus = (props: ChoiceProps) => {
 
     if(!props.showModalConfirmation) return null
 
+    const status = () => {
+        if(props.statusChoice == "Pleine") {
+            return(
+                <p style={{color: "red"}} className="text-center mt-3">Pleine</p>
+            )
+        }
+    }
+
     return(
         <div className="modalConfirmation">
             <div className="modalContainer col-xl-4 text-light">
                 <h3 className="text-center">Signalement</h3>
-                <p className="mt-4 px-3">Vous êtes sur le point de déclarer cette poubelle comme :</p>
-                <p>{props.statusChoice}</p>
+                <p className="mt-4 px-3">Vous êtes sur le point de déclarer cette poubelle comme : {status()}</p>
+                
             </div>
         </div>
     )
