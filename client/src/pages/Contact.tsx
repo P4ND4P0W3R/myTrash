5f05bf0d9d01e1c4f7e97caadbb6d5a70551675c
Thomas94800 <thomascalvo78340@gmail.com>
2024-03-26 18:02:05 +0100

    Contact page Thomas 26/03

---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,20 @@
+import './css/Contact.css';
+
+export default function ContactUs(){
+    return(
+        <section id="contact">    
+            <section id="contacttitle">
+                <h1>CONTACT US!</h1>
+            </section>
+
+            <section id="contactp1">
+                <p> For any question related to the website, please feel free to write to us at <a href="mailto:">mytrash@service.com</a>. </p>
+                <p> We keep ourselves available for any help we can provide you.</p>
+            </section>
+
+            <section id="contactp2">
+                <p> If you wish to invest in our project, do not hesitate to make some donations at <a href="href:">https://donatetomytrash.com</a>. </p>
+            </section>   
+        </section>
+    )
+}   
