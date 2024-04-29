713879f732e1d9f0fe8c7c207f44155dd3b1bb20
Paul HU <94650345+P4ND4P0W3R@users.noreply.github.com>
2024-04-29 16:41:36 +0200

    feat: Create database and API endpoints
    Created the database in PostgreSQL, the API endpoints and the Prisma client

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import { db } from "../prisma";
 
 dotenv.config();
 
@@ -8,6 +9,26 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello, Express with TypeScript!');
+});
+
+app.get('/bins', async (req: Request, res: Response) => {
+    try {
+        const bins = await db.bins.findMany();
+        res.json(bins);
+    } catch (error) {
+        console.error('Error fetching bins:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+app.get('/users', async (req: Request, res: Response) => {
+    try {
+        const users = await db.users.findMany();
+        res.json(users);
+    } catch (error) {
+        console.error('Error fetching users:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.listen(PORT, () => {
