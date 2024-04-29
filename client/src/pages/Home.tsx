b3a3764954db9c6c795a3993138cc330112b6166
nkrief4 <kriefnathan@yahoo.com>
2024-04-29 14:42:15 +0200

    navbar

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,9 +1,38 @@
+import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import './css/Home.css'; // Importez le fichier CSS
 
 
-export default function Home(){
-    return(
+const NavBar = () => {
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    };
+
+    const closeMobileMenu = () => {
+        setIsMobileMenuOpen(false);
+    };
+
+    return (
         <>
-            <h1>Main page</h1>
+            <nav className="navbar">
+                <NavLink className="makeit-logo" to="/">Make It</NavLink>
+                <div className={`nav-links ${isMobileMenuOpen ? 'mobile-menu' : ''}`}>
+                    <ul>
+                        <li><NavLink className="nav-link" to="/" onClick={closeMobileMenu}>Home</NavLink></li>
+                        <li><NavLink className="nav-link" to="/makeit" onClick={closeMobileMenu}>MakeIt</NavLink></li>
+                        <li><NavLink className="nav-link" to="/contact" onClick={closeMobileMenu}>Contact</NavLink></li>
+                        <li><NavLink className="nav-link" to="/logincreate" onClick={closeMobileMenu}>Login/Create</NavLink></li>
+                    </ul>
+                </div>
+                <img src="./src/assets/burger-menu.png" alt="burger-menu" className="burger-menu" onClick={toggleMobileMenu} />
+            </nav>
+            <header></header>
+
+            {/* <router-view></router-view> */}
         </>
-    )
-}
+    );
+};
+
+export default NavBar;
