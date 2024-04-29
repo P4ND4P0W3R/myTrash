a783e7176b2ed4494239f116b883ea1d42fe7279
Cyrilnkl <cyrilnkl5@gmail.com>
2024-04-29 15:39:01 +0200

    user credentials

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,11 +4,20 @@ import Map from './pages/Map.tsx'
 import ContactUs from './pages/Contact.tsx';
 import Login from './pages/Login.tsx';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import {useState} from 'react'
+
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+ const [user, setUserCredentials] = useState(
+   {  
+     email : 'cyrilnkl5@gmail.com',
+     status: 0
+    }
+ )
 
   return (
     <div>
@@ -17,7 +26,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path='/map' element={<Map />} />
           <Route path='/contact' element={<ContactUs />} />
-          <Route path='/login' element={<Login />} />
+          <Route path='/login' element={user.email ? <Navigate to="/" replace /> : <Login />} />
         </Routes>
       </BrowserRouter>
     </div>
