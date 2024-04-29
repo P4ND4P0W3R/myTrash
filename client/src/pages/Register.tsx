799457d851d10ce0adc5edcccc5df9ab75af7560
nkrief4 <kriefnathan@yahoo.com>
2024-04-29 16:31:44 +0200

    navabar sur toutes les pages

---
+++ client/src/pages/Register.tsx
@@ -0,0 +1,14 @@
+import NavBar from "../components/NavBar";
+const Register = () => {
+    return (
+        <>
+        <NavBar />
+        <div>
+            <h1>Register Page</h1>
+            {/* Ajoutez ici le contenu de votre page */}
+        </div>
+        </>
+    );
+};
+
+export default Register;
