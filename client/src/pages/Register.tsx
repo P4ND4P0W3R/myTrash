86a83c938498366fd30f702c3d3bd2d546c3a799
alinahas1311 <a.nahas1311@gmail.com>
2024-05-21 12:09:07 +0200

    aaa

--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -30,44 +30,68 @@ const Register = () => {
 		}
 	};
 
-	return (
-		<>
-			<NavBar />
-			<div className="form-container">
-				<h1>Register</h1>
-				<form onSubmit={register}>
-					<label htmlFor="username">Username:</label>
-					<input
-						type="text"
-						id="username"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-						required
-					/>
-
-					<label htmlFor="email">Email:</label>
-					<input
-						type="email"
-						id="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-						required
-					/>
-
-					<label htmlFor="password">Password:</label>
-					<input
-						type="password"
-						id="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-						required
-					/>
-
-					<button type="submit">Register</button>
-				</form>
-			</div>
-		</>
-	);
+    return (
+        <>
+            <NavBar />
+            <div className="register-container d-flex justify-content-center align-items-center vh-100">
+                <div className="register-card" style={{ borderRadius: '15px' }}>
+                    <div className="card-body mt-1">
+                        <div className="w-100 d-flex justify-content-center align-items-center">
+                            <img src={logo} alt="logo" className="logo" />
+                        </div>
+                        <h5 className="card-title text-center"><strong>Créer un compte myTrash</strong></h5>
+                        <div className="mt-2 text-center">
+                            <p>Déjà un compte? <a href="#" className="connect">Connectez-vous</a></p>
+                            <p className="mb-1">Saisissez vos informations</p>
+                        </div>
+                        <form className="d-flex flex-column align-items-center justify-content-center" onSubmit={register}>
+                            {/* input username */}
+                            <div className="mb-1 m-auto position-relative">
+                                <label className="ms-1" htmlFor="username">Username</label>
+                                <input
+                                    type="text"
+                                    id="username"
+                                    className="form-control"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            {/* input email */}
+                            <div className="mb-1 m-auto position-relative">
+                                <label className="ms-1" htmlFor="email">Email</label>
+                                <input
+                                    type="email"
+                                    id="email"
+                                    className="form-control"
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            {/* input password */}
+                            <div className="mb-1 m-auto position-relative">
+                                <label className="ms-1" htmlFor="password">Password</label>
+                                <input
+                                    type="password"
+                                    id="password"
+                                    className="form-control"
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                    required
+                                />
+                            </div>
+                            <div className="d-flex justify-content-end mt-3 me-3">
+                                <button type="submit" className="btn btn-primary"><strong>Register</strong></button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 };
+
+
 
 export default Register;
