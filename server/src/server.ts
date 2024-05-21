a90c75cb676fd69517c0dbccf406e012decf0444
alinahas1311 <a.nahas1311@gmail.com>
2024-05-21 12:19:57 +0200

    finito

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import cors from 'cors';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json()); // Parse JSON bodies
